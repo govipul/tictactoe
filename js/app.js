282e17053d9302334eeb8f36afc0864929d07804
govipul <vipul_gos@yahoo.co.in>
2017-03-09 10:13:39 +0530

    Grid Created

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('tictacToeApp',[]);
+
+
+app.controller("fullController", ['$scope', function($scope){
+
+  $scope.matrxi = [[-1,-1,-1],
+                   [-1,-1,-1],
+                   [-1,-1,-1]];
+
+  $scope.fillBox = function(){
+
+  };
+}]);
